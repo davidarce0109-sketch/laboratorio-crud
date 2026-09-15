4d0776002747cf3f58a83ae838836cd6c8b3e5e9
david arce <samaritano0109@gmail.com>
2026-09-15 15:54:07 -0500

    frontend

---
+++ Frontend/src/components/navbar.jsx
@@ -0,0 +1,26 @@
+import { link } from 'react-router-dom';
+import { LayoutGrid, LogOut} from 'lucide-react';
+import { useAuth } from '../hooks/useAuth';
+
+export default function Navbar() {
+    const { user, LogOut } = useAuth();
+
+    return (
+        <nav className='navbar'>
+            <link to="/equipos" className="brand">
+                <layoutGrid size={22} />
+                LAB CRUD
+            </link>
+
+            <div className='nav-right'>
+                <span>
+                    {user?.nombre} · <strong>{user?.rol}</strong>
+                </span>
+                <button className='secondary' onClick={LogOut}>
+                    <LogOut size={16} />
+                    Cerrar sesión
+                </button>
+            </div>
+        </nav>
+    );
+}
